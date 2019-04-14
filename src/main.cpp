b94ad0474d03fa9377fb956054862d3de490e305
solomoncotton <47188100+solomoncotton@users.noreply.github.com>
2019-04-14 12:39:04 +0100

    Working Bombs

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,23 @@
 #include "Tiles.h"
 #include "TileList.h"
 #include "Dynamics.h"
+#include "Bomb.h"
 #include <iostream>
 
 
 int main() {
 
-    TileList gameBoard(0);
-    //gameBoard.printBoard();
-    //gameBoard.printBoard();
+    TileList gameBoard(1);
     auto player1 = std::make_shared<Player>(1);
+    auto bomb1 = std::make_shared<Bomb>(rowsize - 2, 1, 4);
+    gameBoard.setObject(rowsize - 2, 1, bomb1);
     gameBoard.addPlayer(player1);
-    std::cout << player1->getX() << player1->getY() << std::endl;
-    //gameBoard.printBoard();
+    gameBoard.printBoard();
 
     while (true) {
         char input = getValidInput();
         player1->move(input, gameBoard);
-        std::cout << player1->getX() << player1->getY() << std::endl;
-        //gameBoard.printBoard();
+        bomb1->reduceTimer(gameBoard);
+        gameBoard.printBoard();
     }
-
-
 }
